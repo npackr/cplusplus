5fcc39a14816ddf1d848576bbd95a709c7219b71
Mã Đằng <npackr@hotmail.com>
2019-01-10 14:58:58 +0700

    Add files via upload

--- Tinh tien lai va ky han.cpp
+++ Tinh tien lai va ky han.cpp
@@ -2,40 +2,6 @@
 #include <cmath>
 using namespace std;
 
-float TinhLaiDon(int goc, int kyvong, double laisuat);
-float TinhLaiKep(int goc, int kyvong, double laisuat);
-
-int main() {
-	int goc, kyvong;
-	double laisuat;
-	
-	cout<<"Nhap so tien ban dang co (don vi: trieu dong): ";
-	cin>>goc;
-	cout<<endl;
-	cout<<"Nhap lai suat (%/nam): ";
-	cin>>laisuat;
-	cout<<endl;
-	cout<<"Nhap so tien ban ky vong (don vi: trieu dong): ";i
-	cin>>kyvong;
-	cout<<endl;
-	
-	cout<<"Ky han gui de dat duoc "<<kyvong<<" trieu dong la:"<<endl;
-	cout<<TinhLaiDon(goc, kyvong, laisuat)<<" nam (neu bang hinh thuc lai don)."<<endl;
-	cout<<TinhLaiKep(goc, kyvong, laisuat)<<" nam (neu bang hinh thuc lai kep).";
-	return 0;
-}
-
-float TinhLaiDon(int goc, int kyvong, double laisuat) {
-	return (kyvong/goc-1)/(laisuat/100);
-}
-
-float TinhLaiKep(int goc, int kyvong, double laisuat) {
-	return log(kyvong/goc)/log(1.0+(laisuat/100));
-}
-#include <iostream>
-#include <cmath>
-using namespace std;
-
 long double TinhLaiDon(int goc, int kyvong, double laisuat);
 long double TinhLaiKep(int goc, int kyvong, double laisuat);
 
